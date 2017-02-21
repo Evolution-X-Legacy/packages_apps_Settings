03ed3f244cee6c7569edbda40c46672963bf7675
Chris Lahaye <dev@chrislahaye.com>
2017-02-21 17:33:16 -0500

    Settings: Add pocket lock toggle
    Change-Id: I398c8f0be23c27cbcf1fb57eed44a801b31bc6c9
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -32,6 +32,7 @@ import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.ColorModePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
+import com.android.settings.display.PocketJudgePreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
 import com.android.settings.display.ProximityOnWakePreferenceController;
@@ -128,6 +129,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(mFontPickerPreference = new FontPickerPreferenceController(context, lifecycle));
         controllers.add(new CameraGesturePreferenceController(context));
         controllers.add(new LiftToWakePreferenceController(context));
+        controllers.add(new PocketJudgePreferenceController(context));
         controllers.add(new NightDisplayPreferenceController(context));
         controllers.add(new NightModePreferenceController(context));
         controllers.add(new ProximityOnWakePreferenceController(context));        
