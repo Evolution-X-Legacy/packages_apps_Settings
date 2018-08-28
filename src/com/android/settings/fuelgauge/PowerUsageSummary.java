a4ff39774bcebf74c7d78fa8bcb6054f49093e34
xyyx <xyyx@mail.ru>
2018-08-28 16:01:57 +0800

    Settings: Remove stock battery percents option
    Change-Id: Ia617b8f714e2fd3dc1ed1073088603c3b1d5fb3e

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -47,7 +47,6 @@ import com.android.settings.Utils;
 import com.android.settings.applications.LayoutPreference;
 import com.android.settings.core.SubSettingLauncher;
 import com.android.settings.dashboard.SummaryLoader;
-import com.android.settings.display.BatteryPercentagePreferenceController;
 import com.android.settings.fuelgauge.anomaly.Anomaly;
 import com.android.settings.fuelgauge.anomaly.AnomalyDetectionPolicy;
 import com.android.settings.fuelgauge.batterytip.BatteryTipLoader;
@@ -265,7 +264,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 KEY_BATTERY_TIP, (SettingsActivity) getActivity(), this /* fragment */, this /*
                 BatteryTipListener */);
         controllers.add(mBatteryTipPreferenceController);
-        controllers.add(new BatteryPercentagePreferenceController(context));
         return controllers;
     }
 
@@ -510,18 +508,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                     return Collections.singletonList(sir);
                 }
 
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> niks = super.getNonIndexableKeys(context);
-
-                    final BatteryPercentagePreferenceController controller =
-                            new BatteryPercentagePreferenceController(context);
-                    if (!controller.isAvailable()) {
-                        niks.add(controller.getPreferenceKey());
-                    }
-                    niks.add(KEY_BATTERY_SAVER_SUMMARY);
-                    return niks;
-                }
             };
 
     public static final SummaryLoader.SummaryProviderFactory SUMMARY_PROVIDER_FACTORY
