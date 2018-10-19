e59718b63b32958a987c038c49ec97971038fce3
raya_lee <Raya_Lee@asus.com>
2018-10-19 18:07:08 +0800

    Add preference for switching to cellular data as Wi-Fi is invalid (Settings)
    Add settings preference for switching to cellular data as Wi-Fi is invalid.
    Add the key "wifi_valid_enable" determine Wi-Fi valid state
    (If Cellular data and Wi-Fi both have internet, 1: always use Wi-Fi 0: auto switch to cellular data)
    1.Avoid user uninformed to use traffic-limited mobile network when connect to invalid Wi-Fi.
    2.Can be connected the Wi-Fi device without network connectivity (eg web hard drive)
    
    Change-Id: I6a25f4e35b3984ca84cd8a7911546c849f0c1e18
    Reviewed-on: http://amax01:8080/18140
    Reviewed-by: Raya Lee <Raya_Lee@asus.com>
    Tested-by: Raya Lee <Raya_Lee@asus.com>
    Reviewed-by: Nick Mou <Nick_Mou@asus.com>

--- src/com/android/settings/wifi/ConfigureWifiSettings.java
+++ src/com/android/settings/wifi/ConfigureWifiSettings.java
@@ -83,6 +83,7 @@ public class ConfigureWifiSettings extends DashboardFragment {
         controllers.add(new WifiInfoPreferenceController(context, getLifecycle(), wifiManager));
         controllers.add(new CellularFallbackPreferenceController(context));
         controllers.add(new WifiP2pPreferenceController(context, getLifecycle(), wifiManager));
+        controllers.add(new WifiCellularSwitchPreferenceController(context));
         controllers.add(new WpsPreferenceController(
                 context, getLifecycle(), wifiManager, getFragmentManager()));
         return controllers;
