0ff3e92f72b93db8fd4817f100bb0514148c312b
bigrushdog <randall.rushing@gmail.com>
2018-11-13 05:13:16 +0000

    Add Font preference for font manager
    Fix some stuff
    
    aex edits:- Add an icon to the preference
    
    Change-Id: Ifb6eb578e6b6b22ca15baa7cb8f8db32bebe6e15
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -38,6 +38,7 @@ import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.display.WallpaperPreferenceController;
+import com.android.settings.display.FontPickerPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -99,6 +100,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new WallpaperPreferenceController(context));
+	    controllers.add(new FontPickerPreferenceController(context, lifecycle, fragment));
         controllers.add(new SystemUiThemePreferenceController(context, lifecycle));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
