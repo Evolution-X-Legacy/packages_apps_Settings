12b3aeff5e1555f0b2f4cde4fff8b6b95a97a71e
Alex <du.alexcruz@gmail.com>
2019-02-01 16:55:15 -0600

    Battery temp: Use MCC to determine the scale type
    The whole tapping on the preference was cute but really not ideal. Most
    users are one or the other and is usually based on where the hell they're
    at. Using the MCC does just that.
    
    Change-Id: I048cda9032a817035c32087cfd9ffeb49003d9d1

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -120,8 +120,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     BatteryTipPreferenceController mBatteryTipPreferenceController;
     private int mStatsType = BatteryStats.STATS_SINCE_CHARGED;
 
-    private boolean batteryTemp = false;
-
     @VisibleForTesting
     LoaderManager.LoaderCallbacks<BatteryInfo> mBatteryInfoLoaderCallbacks =
             new LoaderManager.LoaderCallbacks<BatteryInfo>() {
@@ -228,7 +226,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         restartBatteryInfoLoader();
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
-        updateBatteryTempPreference();
     }
 
     @Override
@@ -240,8 +237,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                         .setTitle(R.string.advanced_battery_title)
                         .launch();
             return true;
-        } else if (KEY_BATTERY_TEMP.equals(preference.getKey())) {
-            updateBatteryTempPreference();
         }
         return super.onPreferenceTreeClick(preference);
     }
@@ -365,9 +360,12 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
         updateLastFullChargePreference();
-        updateBatteryTempPreference();
         mScreenUsagePref.setSubtitle(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
+        mBatteryTemp.setSubtitle(
+                com.android.internal.util.evolution.EvolutionUtils.mccCheck(getContext()) ?
+                com.android.internal.util.evolution.EvolutionUtils.batteryTemperature(getContext(), true) + "°F" :
+                com.android.internal.util.evolution.EvolutionUtils.batteryTemperature(getContext(), false) + "°C");
     }
 
     @VisibleForTesting
@@ -400,19 +398,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
             mLastFullChargePref.setSubtitle(
                     StringUtil.formatRelativeTime(getContext(), lastFullChargeTime,
                             false /* withSeconds */));
-        }
-    }
-
-    @VisibleForTesting
-    void updateBatteryTempPreference() {
-        if (batteryTemp) {
-            mBatteryTemp.setSubtitle(
-                com.android.internal.util.evolution.EvolutionUtils.batteryTemperature(getContext(), false));
-            batteryTemp = false;
-        } else {
-            mBatteryTemp.setSubtitle(
-                com.android.internal.util.evolution.EvolutionUtils.batteryTemperature(getContext(), true));
-            batteryTemp = true;
         }
     }
 
