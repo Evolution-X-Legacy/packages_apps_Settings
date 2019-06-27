AOSiP/platform_packages_apps_Settings

Settings: Adjust search for navigation
 -moved to buttons to use for hardware

Change-Id: I799870b82189ce1988a04905e2c71ead5302741f
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -100,11 +100,11 @@ import com.android.settings.wifi.WifiSettings;
 
 import com.aosip.owlsnest.advanced.SmartPixels;
 import com.aosip.owlsnest.advanced.SystemCategory;
+import com.aosip.owlsnest.buttons.NavigationCategory;
 import com.aosip.owlsnest.buttons.PowermenuCategory;
 import com.aosip.owlsnest.buttons.VolumeCategory;
 import com.aosip.owlsnest.gesture.GestureCategory;
 import com.aosip.owlsnest.lockscreen.OptionsCategory;
-import com.aosip.owlsnest.navigation.NavigationCategory;
 import com.aosip.owlsnest.notification.HeadsUpCategory;
 import com.aosip.owlsnest.notification.NotificationCategory;
 import com.aosip.owlsnest.quicksettings.ExpandedCategory;

Evolution-X-Legacy/packages_apps_Settings

src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -107,6 +107,7 @@ import com.evolution.settings.fragments.ClockDateSettings;
 import com.evolution.settings.fragments.CustomGestureSettings;
 import com.evolution.settings.fragments.LockScreenSettings;
 import com.evolution.settings.fragments.MiscSettings;
+import com.evolution.settings.fragments.NavigationCategory;
 import com.evolution.settings.fragments.NotificationSettings;
 import com.evolution.settings.fragments.PowerMenuSettings;
 import com.evolution.settings.fragments.QuickSettings;
@@ -226,6 +227,7 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(CustomGestureSettings.class);
         addIndex(LockScreenSettings.class);
         addIndex(MiscSettings.class);
+        addIndex(NavigationCategory.class);
         addIndex(NotificationSettings.class);
         addIndex(PowerMenuSettings.class);
         addIndex(QuickSettings.class);